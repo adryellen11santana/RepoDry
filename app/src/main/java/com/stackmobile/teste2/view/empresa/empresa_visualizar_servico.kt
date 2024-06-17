fe6423c1208b57fd5a5de619e87670a9e132ddfa
adryellen11santana <adryellensantana11@gmail.com>
2024-06-17 19:14:30 -0300

    Atualizacao

--- app/src/main/java/com/stackmobile/teste2/view/empresa/empresa_visualizar_servico.kt
+++ app/src/main/java/com/stackmobile/teste2/view/empresa/empresa_visualizar_servico.kt
@@ -2,37 +2,28 @@ package com.stackmobile.teste2.view.empresa
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
-import com.stackmobile.teste2.R
-import com.stackmobile.teste2.databinding.ActivityTelaClienteBinding
-import com.stackmobile.teste2.databinding.ActivityTelaEmpresaBinding
-import com.stackmobile.teste2.view.formlogin.form_login
+import com.stackmobile.teste2.databinding.ActivityEmpresaVisualizarServicoBinding
 import com.stackmobile.teste2.view.home.Home
 
 
-class Tela_empresa : AppCompatActivity() {
+class empresa_visualizar_servico : AppCompatActivity() {
     private  lateinit var recyclerView: RecyclerView
     private lateinit var ordemList: ArrayList<Ordem>
-    lateinit var binding: ActivityTelaEmpresaBinding
+    lateinit var binding: ActivityEmpresaVisualizarServicoBinding
     private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityTelaEmpresaBinding.inflate(layoutInflater)
+        binding = ActivityEmpresaVisualizarServicoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnVoltar.setOnClickListener {
-            val voltarHome = Intent(this, Home::class.java)
+            val voltarHome = Intent(this, Tela_empresa::class.java)
             startActivity(voltarHome)
             finish()
         }
